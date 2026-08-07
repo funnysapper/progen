2bde76cbcd27cdc6cac181b579498496dfc74051
funnysapper <bizzarejacaranda@gmail.com>
2026-08-07 13:20:48 +0000

    another minor change

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(morgan("dev"));           
-app.get('/health', (_req,res)=> {
+app.get('/', (_req,res)=> {
     res.json({status: 'ok'});
 })
 
